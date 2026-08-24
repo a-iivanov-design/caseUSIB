9f9d4a6e372f01228abb3608a64ae6d40e29a5ba
a-iivanov-design <a-iivanov@mail.ru>
2026-08-24 16:07:33 +0300

    Add files via upload

---
+++ public/config (3).js
@@ -0,0 +1,17 @@
+window.APP_CONFIG = {
+    // Логотип картинкой (вставь прямую ссылку на картинку сюда, чтобы вывести как на твоем скриншоте)
+    clubLogoImage: "/logo.jpg", 
+    
+    // Текстовый логотип (если clubLogoImage пустая)
+    clubLogoText: "CASE <span>LOUNGE</span>", 
+
+    // Иконка или ссылка на картинку кейса на главной
+    caseImage: "📦", 
+
+    // Основные цвета киберспортивного стиля
+    primaryColor: "#F5A623",
+    secondaryColor: "#FF4D4D",
+    backgroundColor: "#0A0B0E",
+    cardBackground: "#14161C",
+    borderColor: "#222630"
+};
