6e65252a0e4f79cd69900614b764ee27c7bee05f
a-iivanov-design <a-iivanov@mail.ru>
2026-08-25 17:33:25 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-import express from 'express';
+[source: 30]import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { createClient } from '@libsql/client';
@@ -633,3 +633,4 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
 });
+[source: 30]
