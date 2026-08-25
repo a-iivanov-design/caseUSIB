30fbe008d212e4c3d1a00794a0a6eba08ee17082
a-iivanov-design <a-iivanov@mail.ru>
2026-08-25 16:55:25 +0300

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,17 +1,8 @@
 window.APP_CONFIG = {
-    // Логотип картинкой (оставь пустым "", если хочешь использовать текстовый логотип ниже)
     clubLogoImage: "/logo.jpg", 
-    
-    // Текстовый логотип
     clubLogoText: "CASE <span>LOUNGE</span>", 
-
-    // Иконка или ссылка на картинку кейса на главной
     caseImage: "📦", 
-
-    // Фон главной страницы (ссылка/путь или оставляем пустым "" для стандартного стиля)
     homeBackground: "", 
-
-    // Основные цвета киберспортивного стиля
     primaryColor: "#F5A623",
     secondaryColor: "#FF4D4D",
     backgroundColor: "#0A0B0E",
